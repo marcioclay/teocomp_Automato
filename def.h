2ed23013fd8a99bc5f594ca0677468acfa918997
Márcio Castelo Branco <48799029+marcioclay@users.noreply.github.com>
2026-06-07 16:08:48 -0300

    Create def.h

---
+++ def.h
@@ -0,0 +1,13 @@
+#ifndef EBPF_DEFS_H
+#define EBPF_DEFS_H
+
+#define STATE_Q0   0
+#define STATE_Q1   1
+#define STATE_QERR 2
+
+#define EVENT_CONNECT    1
+#define EVENT_PUBLISH    2
+#define EVENT_DISCONNECT 3
+#define EVENT_ERROR      4
+
+#endif
